86c5cfb4a9d470dd86fd813bba93b758ce552694
Gregg Hamilton <Gregg.Hamilton@linn.co.uk>
2017-05-24 14:47:14 +0100

    Fix implicit conversion from TUint64 to TUint in SpotifyReporter.

--- OpenHome/Media/Pipeline/SpotifyReporter.cpp
+++ OpenHome/Media/Pipeline/SpotifyReporter.cpp
@@ -487,7 +487,7 @@ MsgDecodedStream* SpotifyReporter::CreateMsgDecodedStreamLocked() const
     const DecodedStreamInfo& info = iDecodedStream->StreamInfo();
     // Due to out-of-band track notification from Spotify, audio for current track was probably pushed into pipeline before track offset/duration was known, so use updated values here.
     const TUint64 trackLengthJiffies = TrackLengthJiffiesLocked();
-    const TUint startOffset = iStartOffset.OffsetSample(info.SampleRate());
+    const TUint64 startOffset = iStartOffset.OffsetSample(info.SampleRate());
     MsgDecodedStream* msg =
         iMsgFactory.CreateMsgDecodedStream(info.StreamId(), info.BitRate(), info.BitDepth(),
                                            info.SampleRate(), info.NumChannels(), info.CodecName(),
