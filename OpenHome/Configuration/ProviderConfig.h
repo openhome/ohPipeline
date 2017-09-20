953ca4866f5c4f07cf3480198d32c618453a3d1e
Simon Chisholm <simon.chisholm@linn.co.uk>
2017-09-20 14:21:21 +0100

    We can't sensibly extend the Config service to enable a configuration app (we end up needing two different formats for config keys and it isn't easy to be clear which APIs require which format).  Restore Config service to its previous state; add new functionality to new ConfigApp service.

--- OpenHome/Configuration/ProviderConfig.h
+++ OpenHome/Configuration/ProviderConfig.h
@@ -1,21 +1,12 @@
 #pragma once
 
-#include <Generated/DvAvOpenhomeOrgConfig3.h>
+#include <Generated/DvAvOpenhomeOrgConfig2.h>
 #include <OpenHome/Av/ProviderFactory.h>
 #include <OpenHome/Configuration/ConfigManager.h>
 #include <OpenHome/Buffer.h>
 #include <OpenHome/Json.h>
-#include <OpenHome/Private/Stream.h>
-#include <OpenHome/Private/Thread.h>
-
-#include <map>
 
 namespace OpenHome {
-    namespace Net {
-        class PropertyInt;
-        class PropertyUint;
-        class PropertyString;
-    }
 namespace Configuration {
 
 class KeyWriterJson : public IKeyWriter, private INonCopyable
@@ -28,35 +19,8 @@ private:
     IWriter& iWriter;
 };
 
-class DetailsWriter
-{
-    static const TUint kBufGranularity;
-    static const Brn kKeyKey;
-    static const Brn kKeyType;
-    static const Brn kKeyMeta;
-    static const Brn kKeyReboot;
-    static const Brn kValTypeNum;
-    static const Brn kValTypeChoice;
-    static const Brn kValTypeText;
-    static const Brn kKeyNumMin;
-    static const Brn kKeyNumMax;
-    static const Brn kKeyNumDefault;
-    static const Brn kKeyEnumVals;
-    static const Brn kKeyTextLen;
-public:
-    DetailsWriter();
-    void Add(ConfigNum& aVal);
-    void Add(ConfigChoice& aVal);
-    void Add(ConfigText& aVal);
-    const Brx& Flush();
-private:
-    WriterBwh iWriterBuf;
-    WriterJsonArray iWriterArray;
-};
-
-class ProviderConfig : public Net::DvProviderAvOpenhomeOrgConfig3
+class ProviderConfig : public Net::DvProviderAvOpenhomeOrgConfig2
                      , public Av::IProvider
-                     , private IConfigObserver
 {
     static const TUint kErrorCodeInvalidKey;
     static const TUint kErrorCodeNotANumber;
@@ -69,73 +33,15 @@ class ProviderConfig : public Net::DvProviderAvOpenhomeOrgConfig3
     static const Brn kErrorDescInvalidSelection;
     static const Brn kErrorDescValueTooLong;
 public:
-    ProviderConfig(Net::DvDevice& aDevice,
-                   Configuration::IConfigManager& aConfigManager,
-                   Configuration::IConfigObservable& aConfigObservable,
-                   Configuration::IStoreReadWrite& aStore);
-    ~ProviderConfig();
-    void Start();
-private: // from IConfigObserver
-    void Added(ConfigNum& aVal) override;
-    void Added(ConfigChoice& aVal) override;
-    void Added(ConfigText& aVal) override;
-    void Removed(ConfigNum& aVal) override;
-    void Removed(ConfigChoice& aVal) override;
-    void Removed(ConfigText& aVal) override;
-private:
-    void ConfigNumChanged(KeyValuePair<TInt>& aKvp);
-    void ConfigChoiceChanged(KeyValuePair<TUint>& aKvp);
-    void ConfigTextChanged(KeyValuePair<const Brx&>& aKvp);
-    void ClearMaps();
+    ProviderConfig(Net::DvDevice& aDevice, Configuration::IConfigManager& aConfigManager);
 private: // from DvProviderAvOpenhomeOrgConfiguration1
     void GetKeys(Net::IDvInvocation& aInvocation, Net::IDvInvocationResponseString& aKeyList) override;
-    void GetDetails(Net::IDvInvocation& aInvocation, Net::IDvInvocationResponseString& aKeys) override;
     void SetValue(Net::IDvInvocation& aInvocation, const Brx& aKey, const Brx& aValue) override;
     void GetValue(Net::IDvInvocation& aInvocation, const Brx& aKey, Net::IDvInvocationResponseString& aValue) override;
     void HasKey(Net::IDvInvocation& aInvocation, const Brx& aKey, Net::IDvInvocationResponseBool& aValue) override;
-    void ResetAll(Net::IDvInvocation& aInvocation) override;
-private:
-    class ConfigItemNum
-    {
-    public:
-        ConfigItemNum(ConfigNum& aConfigNum, Net::PropertyInt& aProperty);
-        ~ConfigItemNum();
-    public:
-        ConfigNum& iVal;
-        Net::PropertyInt& iProperty;
-        TUint iListenerId;
-    };
-    class ConfigItemChoice
-    {
-    public:
-        ConfigItemChoice(ConfigChoice& aConfigChoice, Net::PropertyUint& aProperty);
-        ~ConfigItemChoice();
-    public:
-        ConfigChoice& iVal;
-        Net::PropertyUint& iProperty;
-        TUint iListenerId;
-    };
-    class ConfigItemText
-    {
-    public:
-        ConfigItemText(ConfigText& aConfigText, Net::PropertyString& aProperty);
-        ~ConfigItemText();
-    public:
-        ConfigText& iVal;
-        Net::PropertyString& iProperty;
-        TUint iListenerId;
-    };
 private:
     Configuration::IConfigManager& iConfigManager;
-    Configuration::IConfigObservable& iConfigObservable;
-    IStoreReadWrite& iStore;
-    DetailsWriter iDetailsWriter;
-    std::map<Brn, ConfigItemNum*, BufferCmp> iMapNum;
-    std::map<Brn, ConfigItemChoice*, BufferCmp> iMapChoice;
-    std::map<Brn, ConfigItemText*, BufferCmp> iMapText;
-    Mutex iLock;
 };
 
 }  // namespace Configuration
 }  // namespace OpenHome
-
---
+++ OpenHome/Configuration/ProviderConfigApp.h
@@ -0,0 +1,122 @@
+#pragma once
+
+#include <Generated/DvAvOpenhomeOrgConfigApp1.h>
+#include <OpenHome/Av/ProviderFactory.h>
+#include <OpenHome/Configuration/ConfigManager.h>
+#include <OpenHome/Buffer.h>
+#include <OpenHome/Json.h>
+#include <OpenHome/Private/Stream.h>
+#include <OpenHome/Private/Thread.h>
+
+#include <map>
+
+namespace OpenHome {
+    namespace Net {
+        class PropertyInt;
+        class PropertyUint;
+        class PropertyString;
+    }
+namespace Configuration {
+
+class ProviderConfigApp : public Net::DvProviderAvOpenhomeOrgConfigApp1
+                        , public Av::IProvider
+                        , private IConfigObserver
+{
+    static const TUint kErrorCodeInvalidKey;
+    static const TUint kErrorCodeNotANumber;
+    static const TUint kErrorCodeValueOutOfRange;
+    static const TUint kErrorCodeInvalidSelection;
+    static const TUint kErrorCodeValueTooLong;
+    static const Brn kErrorDescInvalidKey;
+    static const Brn kErrorDescNotANumber;
+    static const Brn kErrorDescValueOutOfRange;
+    static const Brn kErrorDescInvalidSelection;
+    static const Brn kErrorDescValueTooLong;
+public:
+    ProviderConfigApp(Net::DvDevice& aDevice,
+                      Configuration::IConfigManager& aConfigManager,
+                      Configuration::IConfigObservable& aConfigObservable,
+                      Configuration::IStoreReadWrite& aStore);
+    ~ProviderConfigApp();
+private: // from IConfigObserver
+    void Added(ConfigNum& aVal) override;
+    void Added(ConfigChoice& aVal) override;
+    void Added(ConfigText& aVal) override;
+    void AddsComplete() override;
+    void Removed(ConfigNum& aVal) override;
+    void Removed(ConfigChoice& aVal) override;
+    void Removed(ConfigText& aVal) override;
+private:
+    void CreateKey(const Brx& aConfigKey, Bwx& aKey);
+    void ConfigNumChanged(KeyValuePair<TInt>& aKvp);
+    void ConfigChoiceChanged(KeyValuePair<TUint>& aKvp);
+    void ConfigTextChanged(KeyValuePair<const Brx&>& aKvp);
+    void ClearMaps();
+private: // from DvProviderAvOpenhomeOrgConfiguration1
+    void GetKeys(Net::IDvInvocation& aInvocation, Net::IDvInvocationResponseString& aKeys) override;
+    void SetValue(Net::IDvInvocation& aInvocation, const Brx& aKey, const Brx& aValue) override;
+    void GetValue(Net::IDvInvocation& aInvocation, const Brx& aKey, Net::IDvInvocationResponseString& aValue) override;
+    void ResetAll(Net::IDvInvocation& aInvocation) override;
+private:
+    class KeysWriter
+    {
+        static const TUint kBufGranularity;
+        static const Brn kKeyKey;
+        static const Brn kKeyType;
+        static const Brn kKeyMeta;
+        static const Brn kKeyReboot;
+        static const Brn kValTypeNum;
+        static const Brn kValTypeChoice;
+        static const Brn kValTypeText;
+        static const Brn kKeyNumMin;
+        static const Brn kKeyNumMax;
+        static const Brn kKeyNumDefault;
+        static const Brn kKeyEnumVals;
+        static const Brn kKeyTextLen;
+    public:
+        KeysWriter();
+        void Add(ConfigNum& aVal, const Brx& aKey);
+        void Add(ConfigChoice& aVal, const Brx& aKey);
+        void Add(ConfigText& aVal, const Brx& aKey);
+        const Brx& Flush();
+    private:
+        WriterBwh iWriterBuf;
+        WriterJsonArray iWriterArray;
+    };
+    template <class T, class S> class ConfigItem
+    {
+    public:
+        ConfigItem(T& aConfigVal, S& aProperty, Bwh& aKey)
+            : iVal(aConfigVal)
+            , iProperty(aProperty)
+            , iListenerId(IConfigManager::kSubscriptionIdInvalid)
+        {
+            aKey.TransferTo(iKey);
+        }
+        ~ConfigItem()
+        {
+            iVal.Unsubscribe(iListenerId);
+        }
+    public:
+        T& iVal;
+        S& iProperty;
+        TUint iListenerId;
+        Brh iKey;
+    };
+    typedef ConfigItem<ConfigNum,    Net::PropertyInt>    ConfigItemNum;
+    typedef ConfigItem<ConfigChoice, Net::PropertyUint>   ConfigItemChoice;
+    typedef ConfigItem<ConfigText,   Net::PropertyString> ConfigItemText;
+private:
+    Configuration::IConfigManager& iConfigManager;
+    Configuration::IConfigObservable& iConfigObservable;
+    IStoreReadWrite& iStore;
+    KeysWriter iKeysWriter;
+    std::map<Brn, ConfigItemNum*, BufferCmp> iMapNum;
+    std::map<Brn, ConfigItemChoice*, BufferCmp> iMapChoice;
+    std::map<Brn, ConfigItemText*, BufferCmp> iMapText;
+    Mutex iLock;
+};
+
+}  // namespace Configuration
+}  // namespace OpenHome
+
