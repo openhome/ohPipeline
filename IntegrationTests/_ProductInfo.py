f599e4b07fa148fad78c971398ee292be2f1549a
grahamdo <graham.douglas@linn.co.uk>
2018-03-12 13:06:27 +0000

    Updates to Scd testing

---
+++ IntegrationTests/_ProductInfo.py
@@ -0,0 +1,34 @@
+"""_ProductInfo - product specific information (for OHMP)
+"""
+
+
+class ProductInfo:
+
+    def __init__( self, aDut ):
+        pass
+
+    #
+    # 'Features' which are only available on certain devices
+    #
+
+    @property
+    def exaktOnly( self ):
+        return False
+
+    @property
+    def noPreOut( self ):
+        return True
+
+    @property
+    def digitalAmps( self ):
+        """Products with digital amps"""
+        return False
+
+    @property
+    def inputLevelAdjust( self ):
+        """Products with adjustable input level (on analog inputs)"""
+        return False
+
+    @property
+    def dsdPlayback( self ):
+        return True
