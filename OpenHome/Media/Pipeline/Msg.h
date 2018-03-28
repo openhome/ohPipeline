d982280e8bdfa3b14e19dac5fa0ff6ff5708c546
Jim Robinson <jim.robinson@linn.co.uk>
2018-03-28 09:41:37 +0100

    DAE82: Increased message size to accommodate 192k 6 channel

--- OpenHome/Media/Pipeline/Msg.h
+++ OpenHome/Media/Pipeline/Msg.h
@@ -113,7 +113,7 @@ enum class AudioDataEndian
 class AudioData : public Allocated
 {
 public: 
-    static const TUint kMaxBytes = 8208; // max of 8k (DSD), 2ms/10ch/96/32 and 5ms/2ch/192/24
+    static const TUint kMaxBytes = 9216; // max of 8k (DSD), 2ms/6ch/192/32 and 5ms/2ch/192/24
                                          // (latter for Songcast, supporting earliest receiver)
                                          // ...rounded up to allow full utilisation for 16, 24
                                          // and 32-bit audio
