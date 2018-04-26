fa9a50f017b2a52613b65bc4dc8bef1f265ce3ce
grahamdo <graham.douglas@linn.co.uk>
2018-04-26 13:37:25 +0100

    More tries to get publishing

--- projectdata/build_behaviour.py
+++ projectdata/build_behaviour.py
@@ -60,7 +60,7 @@ import shutil
 from ci import (
         build_step, require_version, add_option, specify_optional_steps,
         build_condition, default_platform, get_dependency_args,
-        get_vsvars_environment, fetch_dependencies, python, OpenHomeBuilder)
+        get_vsvars_environment, fetch_dependencies, python, scp)
 import platform
 
 require_version(51)
@@ -210,5 +210,4 @@ def integration_test_full(context):
 def publish(context):
     targetpath    = "{OH_PUBLISHDIR}/{OH_PROJECT}/{OH_PROJECT}-{OH_VERSION}-{OH_PLATFORM}-{OH_DEBUG}.tar.gz".format(**context.env)
     sourcepath    = "{BUILDDIR}/{OH_PROJECT}.tar.gz".format(**context.env)
-    o = OpenHomeBuilder()
-    o.publish_package(sourcepath, targetpath)
+    scp(sourcepath,    targetpath)
