107652a96cb42f6c725f12c386effb0f82900ca6
Ewan <ewan.mccartney@linn.co.uk>
2020-07-29 11:23:52 +0100

    Debug: Add a minor sleep into some Mac OSX tests to make them pass more reliably with SleepWake

--- OpenHome/Av/Tests/TestStoreMain.cpp
+++ OpenHome/Av/Tests/TestStoreMain.cpp
@@ -5,7 +5,18 @@ extern void TestStore();
 void OpenHome::TestFramework::Runner::Main(TInt /*aArgc*/, TChar* /*aArgv*/[], Net::InitialisationParams* aInitParams)
 {
     Net::UpnpLibrary::InitialiseMinimal(aInitParams);
+
     TestStore();
-    delete aInitParams;
+
+#ifdef MAC_OSX
+    /* Mac internally uses CFRunLoops for SleepWake threads.
+     * We need to be careful not to do things too quickly 
+     * otherwise the CF functions we use will race and 
+     * crash the test runs */
+    Thread::Sleep(500);
+#endif
+
     Net::UpnpLibrary::Close();
+
+    delete aInitParams;
 }
--- OpenHome/Media/Tests/TestMuteManagerMain.cpp
+++ OpenHome/Media/Tests/TestMuteManagerMain.cpp
@@ -8,7 +8,18 @@ extern void TestMuteManager();
 void OpenHome::TestFramework::Runner::Main(TInt /*aArgc*/, TChar* /*aArgv*/[], Net::InitialisationParams* aInitParams)
 {
     Net::UpnpLibrary::InitialiseMinimal(aInitParams);
+
     TestMuteManager();
-    delete aInitParams;
+
+#ifdef MAC_OSX
+    /* Mac internally uses CFRunLoops for SleepWake threads.
+     * We need to be careful not to do things too quickly 
+     * otherwise the CF functions we use will race and 
+     * crash the test runs */
+    Thread::Sleep(500);
+#endif
+
     Net::UpnpLibrary::Close();
+
+    delete aInitParams;
 }
