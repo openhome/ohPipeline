openhome/ohPipeline

commit d3280fa48581cac85460dfc87908bba78f58e8ab

[TIDAL] Fix possible crash when logging stream errors
As first noticed in Airable work, repeatedly skipping a track can cause
a crash. This happens when trying to read contents from an interupted
socket.

OpenHome/Av/Tidal/ProtocolTidal.cpp
@@ -524,7 +524,11 @@ ProtocolStreamResult ProtocolTidal::DoStream()
         const TUint bytesToRead = static_cast<TUint>(std::min(iTotalBytes, static_cast<TUint64>(kMaxErrorReadBytes)));
 
         try {
-            while(iErrorBuf.Bytes() < bytesToRead) {
+
+            // We break on 'Stopped' which is set if this is interupted.
+            // This ensures that if we are interupted while processing an error
+            // we don't attempt to read from an interupted socket.
+            while(iErrorBuf.Bytes() < bytesToRead && !iStopped) {
                 const TUint bytesLeft = bytesToRead - iErrorBuf.Bytes();
                 iErrorBuf.Append(iReaderUntil.Read(bytesLeft));
             }
@@ -533,11 +537,16 @@ ProtocolStreamResult ProtocolTidal::DoStream()
             // If we do't have enough (or any) of additional error information, it's not the end of the world.
         }
 
-        if (iErrorBuf.Bytes() > 0) {
-            LOG_ERROR(kPipeline, "ProtocolTidal::DoStream server returned error %u\nSome (or all) of the response is:\n%.*s\n", code, PBUF(iErrorBuf))
-        }
-        else {
-            LOG_ERROR(kPipeline, "ProtocolTidal::DoStream server returned error %u\n", code);
+        // If we have been stopped it's unlikely we want to see any error that occurs as the DS
+        // wil have moved on to something else.
+        if (!iStopped)
+        {
+            if (iErrorBuf.Bytes() > 0) {
+                LOG_ERROR(kPipeline, "ProtocolTidal::DoStream server returned error %u\nSome (or all) of the response is:\n%.*s\n", code, PBUF(iErrorBuf))
+            }
+            else {
+                LOG_ERROR(kPipeline, "ProtocolTidal::DoStream server returned error %u\n", code);
+            }
         }
 
         return EProtocolStreamErrorUnrecoverable;

commit 7922aa9be317fd04dda98cea5760a726969b0dfa

[TIDAL] Prevent reading from an unconnected socket
Commits 47ae2153 and d3280fa4 has introduced a logging pathway for
streaming errors in TIDAL code. Unfortunately both HTTP and Socket
errors are communicated back to callers in the same manner.

In the case a socket failed to connect, we'd then attempt to read from
buffers that were either in an invalid state or referencing a previous
network request. This usually manifested itself as an assert or a seg
fault.

See #7721 for more details.

OpenHome/Av/Tidal/ProtocolTidal.cpp
@@ -512,29 +512,37 @@ TBool ProtocolTidal::IsCurrentStream(TUint aStreamId) const
 
 ProtocolStreamResult ProtocolTidal::DoStream()
 {
-    TUint code = WriteRequest(0);
+    const TUint code = WriteRequest(0);
     iSeekable = false;
     iTotalBytes = iHeaderContentLength.ContentLength();
 
-    if (code != HttpStatus::kPartialContent.Code() && code != HttpStatus::kOk.Code()) {
-        iErrorBuf.SetBytes(0);
+    const TBool wasSuccessStatusCode = code == HttpStatus::kPartialContent.Code()
+                                        || code == HttpStatus::kOk.Code();
 
-        // Casting the kMaxErrorReadBytes is required as std::min takes references, but constant values
-        // are inlined by default which breaks compilation
-        const TUint bytesToRead = static_cast<TUint>(std::min(iTotalBytes, static_cast<TUint64>(kMaxErrorReadBytes)));
+    if (!wasSuccessStatusCode)
+    {
+        // We should only attempt to read anything if the socket has actually completed a connection
+        if (iSocket.IsConnected())
+        {
+            iErrorBuf.SetBytes(0);
 
-        try {
+            // Casting the kMaxErrorReadBytes is required as std::min takes references, but constant values
+            // are inlined by default which breaks compilation
+            const TUint bytesToRead = static_cast<TUint>(std::min(iTotalBytes, static_cast<TUint64>(kMaxErrorReadBytes)));
 
-            // We break on 'Stopped' which is set if this is interupted.
-            // This ensures that if we are interupted while processing an error
-            // we don't attempt to read from an interupted socket.
-            while(iErrorBuf.Bytes() < bytesToRead && !iStopped) {
-                const TUint bytesLeft = bytesToRead - iErrorBuf.Bytes();
-                iErrorBuf.Append(iReaderUntil.Read(bytesLeft));
+            try {
+
+                // We break on 'Stopped' which is set if this is interupted.
+                // This ensures that if we are interupted while processing an error
+                // we don't attempt to read from an interupted socket.
+                while(iErrorBuf.Bytes() < bytesToRead && !iStopped) {
+                    const TUint bytesLeft = bytesToRead - iErrorBuf.Bytes();
+                    iErrorBuf.Append(iReaderUntil.Read(bytesLeft));
+                }
             }
-        }
-        catch (ReaderError&){
-            // If we do't have enough (or any) of additional error information, it's not the end of the world.
+            catch (ReaderError&){
+                // If we do't have enough (or any) of additional error information, it's not the end of the world.
+            }
         }
 
         // If we have been stopped it's unlikely we want to see any error that occurs as the DS