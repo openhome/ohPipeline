c7cba6f15028ce12db2ee1a8d111c5c33d80915d
Ewan <git@ewan.emccartney.co.uk>
2022-08-26 09:38:28 +0100

    TIDAL: Convert pins to use standard DIDLWriter class
    This patch removes the custom DIDL-Lite writing code from inside TIDAL
    pins. By doing so, we are able to remove a large amount of duplicated
    code.
    
    We also can take advantage of the restructuring an do less parsing work,
    particularly when adding artworks. Previously, for each type of artwork,
    we had to re-parse the item ID, do some formatting for each artwork
    resolution. Now, we do that work once and instead do some buffer
    operations. In theory this should be a lot quicker than before.
    
    Additionally, we now report the type of exception if one is thrown when
    turning TIDAL metadata into DIDL-Lite.

--- OpenHome/Av/Tidal/TidalMetadata.cpp
+++ OpenHome/Av/Tidal/TidalMetadata.cpp
@@ -10,58 +10,9 @@
 #include <OpenHome/Private/Parser.h>
 #include <OpenHome/Av/Pins/Pins.h>
 
-namespace OpenHome {
-namespace Av {
-
-class Tidal2DidlTagMapping
-{
-public:
-    Tidal2DidlTagMapping(const TChar* aTidalKey, const TChar* aDidlTag, const OpenHome::Brx& aNs)
-        : iTidalKey(aTidalKey)
-        , iDidlTag(aDidlTag)
-        , iNs(aNs)
-        , iRole(OpenHome::Brx::Empty())
-        , iTidalSubKey(OpenHome::Brx::Empty())
-        , iIsImage(false)
-        , iResolution(TidalMetadata::eNoImage)
-    {}
-    Tidal2DidlTagMapping(const TChar* aTidalKey, const TChar* aDidlTag, const OpenHome::Brx& aNs, const TChar* aRole)
-        : iTidalKey(aTidalKey)
-        , iDidlTag(aDidlTag)
-        , iNs(aNs)
-        , iRole(aRole)
-        , iTidalSubKey(OpenHome::Brx::Empty())
-        , iIsImage(false)
-        , iResolution(TidalMetadata::eNoImage)
-    {}
-    Tidal2DidlTagMapping(const TChar* aTidalKey, const TChar* aDidlTag, const OpenHome::Brx& aNs, const TChar* aSubKey, TBool aIsImage, TidalMetadata::EImageResolution aResolution)
-        : iTidalKey(aTidalKey)
-        , iDidlTag(aDidlTag)
-        , iNs(aNs)
-        , iRole(OpenHome::Brx::Empty())
-        , iTidalSubKey(aSubKey)
-        , iIsImage(aIsImage)
-        , iResolution(aResolution)
-    {}
-public:
-    OpenHome::Brn iTidalKey;
-    OpenHome::Brn iDidlTag;
-    OpenHome::Brn iNs;
-    OpenHome::Brn iRole;
-    OpenHome::Brn iTidalSubKey;
-    TBool iIsImage;
-    TidalMetadata::EImageResolution iResolution;
-};
-
-} // namespace Av
-} // namespace OpenHome
-
 using namespace OpenHome;
 using namespace OpenHome::Av;
 
-const Brn TidalMetadata::kNsDc("dc=\"http://purl.org/dc/elements/1.1/\"");
-const Brn TidalMetadata::kNsUpnp("upnp=\"urn:schemas-upnp-org:metadata-1-0/upnp/\"");
-const Brn TidalMetadata::kNsOh("oh=\"http://www.openhome.org\"");
 const Brn TidalMetadata::kImageResourceBaseUrl("https://resources.tidal.com/images/");
 const Brn TidalMetadata::kImageResourceResolutionLow("320x320"); // 80x80 | 160x160 | 320x320 | 640x640 | 750x750 | 1080x1080 | 1280x1280
 const Brn TidalMetadata::kImageResourceResolutionMed("640x640");
@@ -100,8 +51,8 @@ Media::Track* TidalMetadata::TrackFromJson(const Brx& aMetadata,
     catch (AssertionFailed&) {
         throw;
     }
-    catch (Exception&) {
-        LOG_ERROR(kMedia, "TidalMetadata::TrackFromJson failed to parse metadata - trackBytes=%u\n", iTrackUri.Bytes());
+    catch (Exception& ex) {
+        LOG_ERROR(kMedia, "TidalMetadata::TrackFromJson failed to parse metadata (%s) - trackBytes=%u\n", ex.Message(), iTrackUri.Bytes());
         if (iTrackUri.Bytes() > 0) {
             return iTrackFactory.CreateTrack(iTrackUri, Brx::Empty());
         }
@@ -112,36 +63,18 @@ Media::Track* TidalMetadata::TrackFromJson(const Brx& aMetadata,
 void TidalMetadata::ParseTidalMetadata(const Brx& aMetadata,
                                        const Brx& aTokenId)
 {
-    static const Tidal2DidlTagMapping kTidal2Didl[] ={
-        { "title", "dc:title", kNsDc },
-        { "trackNumber", "upnp:originalTrackNumber", kNsUpnp },
-    };
-    static const TUint kNumTidal2DidlMappings = sizeof kTidal2Didl / sizeof kTidal2Didl[0];
-
-    //static const Tidal2DidlTagMapping kTidalArray2Didl[] ={
-        //{ "artists", "upnp:artist", kNsUpnp, "" }, ??
-    //};
-    //static const TUint kNumTidalArray2DidlMappings = sizeof kTidalArray2Didl / sizeof kTidalArray2Didl[0];
-
-    static const Tidal2DidlTagMapping kTidalObj2Didl[] ={
-        { "album", "upnp:albumArtURI", kNsUpnp, "cover", true, eLow },
-        { "album", "upnp:albumArtURI", kNsUpnp, "cover", true, eMed },
-        { "album", "upnp:albumArtURI", kNsUpnp, "cover", true, eHigh },
-        { "album", "upnp:album", kNsUpnp, "title", false, eNoImage },
-        { "artist", "upnp:artist", kNsUpnp, "name", false, eNoImage },
-    };
-    static const TUint kNumTidalObj2DidlMappings = sizeof kTidalObj2Didl / sizeof kTidalObj2Didl[0];
-
     iTrackUri.Replace(Brx::Empty());
     iMetaDataDidl.Replace(Brx::Empty());
     JsonParser parser;
+    JsonParser nestedParser;
     parser.Parse(aMetadata);
 
     if (parser.HasKey("item")) {
         // playlists have an extra layer of indirection (item dictionary) as they can be mixed media (audio and video)
         parser.Parse(parser.String("item"));
     }
-    else if (!parser.HasKey("id")) {
+
+    if (!parser.HasKey("id")) {
         // track uri based on id, so will be invalid without one
         THROW(TidalResponseInvalid);
     }
@@ -159,189 +92,93 @@ void TidalMetadata::ParseTidalMetadata(const Brx& aMetadata,
     //if (parser.HasKey("url")) { // streamable tidal url
     //    iTrackUri.ReplaceThrow(parser.String("url")); 
     //}
-    if (parser.HasKey("id")) { // special linn style tidal url (non-streamable, gets converted later)
 
-        iTrackUri.ReplaceThrow(Brn("tidal://track?trackId="));
-        iTrackUri.AppendThrow(parser.String("id"));
-        iTrackUri.AppendThrow(Brn("&version="));
+     // special linn style tidal url (non-streamable, gets converted later)
+    const Brx& itemId = parser.String("id");
+    iTrackUri.ReplaceThrow(Brn("tidal://track?trackId="));
+    iTrackUri.AppendThrow(itemId);
+    iTrackUri.AppendThrow(Brn("&version="));
 
-        const TBool isUsingOAuth = aTokenId.Bytes() > 0;
+    const TBool isUsingOAuth = aTokenId.Bytes() > 0;
 
-        if (isUsingOAuth)
-        {
-            iTrackUri.AppendThrow("2&token=");
-            iTrackUri.AppendThrow(aTokenId);
+    if (isUsingOAuth)
+    {
+        iTrackUri.AppendThrow("2&token=");
+        iTrackUri.AppendThrow(aTokenId);
+    }
+    else
+    {
+        iTrackUri.AppendThrow("1");
+    }
+
+    Bwn unescapedBuf;
+    auto unescapeVal = [&] (const Brx& aValue) {
+        unescapedBuf.Set(aValue.Ptr(), aValue.Bytes(), aValue.Bytes());
+        Json::Unescape(unescapedBuf);
+    };
+
+    WriterDIDLLite writer(itemId, DIDLLite::kItemTypeTrack, iMetaDataDidl);
+
+    if (parser.HasKey("title")) {
+        unescapeVal(parser.String("title"));
+        writer.WriteTitle(unescapedBuf);
+    }
+
+    if (parser.HasKey("trackNumber")) {
+        unescapeVal(parser.String("trackNumber"));
+        writer.WriteTrackNumber(unescapedBuf);
+    }
+
+    if (parser.HasKey("album")) {
+        nestedParser.Parse(parser.String("album"));
+        if (nestedParser.HasKey("title")) {
+            unescapeVal(nestedParser.String("title"));
+            writer.WriteAlbum(unescapedBuf);
         }
-        else
-        {
-            iTrackUri.AppendThrow("1");
+
+        TryWriteArtwork(writer, nestedParser);
+    }
+
+    if (parser.HasKey("artist")) {
+        nestedParser.Parse(parser.String("artist"));
+        if (nestedParser.HasKey("name")) {
+            unescapeVal(nestedParser.String("name"));
+            writer.WriteArtist(unescapedBuf);
         }
     }
-    TryAppend("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-    TryAppend("<DIDL-Lite xmlns=\"urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/\">");
-    TryAppend("<item");
-    TryAddAttribute(parser, "id", "id");
-    TryAppend(" parentID=\"-1\" restricted=\"1\">");
-    for (TUint i=0; i<kNumTidal2DidlMappings; i++) {
-        auto& mapping = kTidal2Didl[i];
-        TryAddTag(parser, mapping.iTidalKey, mapping.iDidlTag, mapping.iNs);
-    }
-    TryAddTag(Brn("upnp:class"), kNsUpnp, Brx::Empty(), Brn("object.item.audioItem.musicTrack"));
-    for (TUint i=0; i<kNumTidalObj2DidlMappings; i++) {
-        auto& mapping = kTidalObj2Didl[i];
-        TryAddTagsFromObj(parser, mapping.iTidalKey, mapping.iDidlTag, mapping.iNs, mapping.iTidalSubKey, mapping.iIsImage, mapping.iResolution);
-    }
-    //for (TUint i=0; i<kNumTidalArray2DidlMappings; i++) {
-    //    auto& mapping = kTidalArray2Didl[i];
-    //    TryAddTagsFromArray(parser, mapping.iTidalKey, mapping.iDidlTag, mapping.iNs, mapping.iRole);
-    //}
-    TryAppend("<res");
-    TryAddAttribute("http-get:*:*:*", "protocolInfo");
-    if (parser.HasKey("duration")) {
-        TryAppend(" duration=\"");
-        TUint duration = Ascii::Uint(parser.String("duration"));
-        const TUint secs = duration % 60;
-        duration /= 60;
-        const TUint mins = duration % 60;
-        const TUint hours = duration / 60;
-        Bws<32> formatted;
-        formatted.AppendPrintf("%u:%02u:%02u.000", hours, mins, secs);
-        TryAppend(formatted);
-        TryAppend("\"");
-    }
-    
-    TryAppend(">");
-    if (iTrackUri.Bytes() > 0) {
-        WriterBuffer writer(iMetaDataDidl);
-        Converter::ToXmlEscaped(writer, iTrackUri);
-    }
-    TryAppend("</res>");
-    TryAppend("</item>");
-    TryAppend("</DIDL-Lite>");
+
+    writer.WriteStreamingDetails(DIDLLite::kProtocolHttpGet, parser.Num("duration"), iTrackUri);
+    writer.WriteEnd();
 }
 
-void TidalMetadata::TryAddAttribute(JsonParser& aParser, const TChar* aTidalKey, const TChar* aDidlAttr)
+void TidalMetadata::TryWriteArtwork(WriterDIDLLite& aWriter, JsonParser& aParser)
 {
-    if (aParser.HasKey(aTidalKey)) {
-        TryAppend(" ");
-        TryAppend(aDidlAttr);
-        TryAppend("=\"");
-        TryAppend(aParser.String(aTidalKey));
-        TryAppend("\"");
-    }
-}
-
-void TidalMetadata::TryAddAttribute(const TChar* aValue, const TChar* aDidlAttr)
-{
-    TryAppend(" ");
-    TryAppend(aDidlAttr);
-    TryAppend("=\"");
-    TryAppend(aValue);
-    TryAppend("\"");
-}
-
-void TidalMetadata::TryAddTagsFromObj(JsonParser& aParser,
-                                     const Brx& aTidalKey, const Brx& aDidlTag,
-                                     const Brx& aNs, const Brx& aTidalSubKey, TBool aIsImage, EImageResolution aResolution)
-{
-    if (!aParser.HasKey(aTidalKey)) {
+    // NOTE: Assumes 'aParser' is already pointing to a valid TIDAL album object
+    if (!aParser.HasKey("cover")) {
         return;
     }
-    JsonParser nestedParser;
-    nestedParser.Parse(aParser.String(aTidalKey));
-    Bwh val(1024);
-    Bwn valEscaped;
-    if (aIsImage) {
-        val.ReplaceThrow(kImageResourceBaseUrl);
-        Parser idParser(nestedParser.String(aTidalSubKey));
-        while (!idParser.Finished()) {
-            val.AppendThrow(idParser.Next('-')); // replace '-' with '/' in value
-            val.AppendThrow(Brn("/"));
-        }
-        if (aResolution == eLow) {
-            val.AppendThrow(kImageResourceResolutionLow);
-        }
-        else if (aResolution == eMed) {
-            val.AppendThrow(kImageResourceResolutionMed);
-        }
-        else {
-            val.AppendThrow(kImageResourceResolutionHigh);
-        }
-        val.AppendThrow(kImageResourceExtension);
-    }
-    else {
-        val.ReplaceThrow(nestedParser.String(aTidalSubKey));
-    }
-    valEscaped.Set(val.Ptr(), val.Bytes(), val.Bytes());
-    Json::Unescape(valEscaped);
-    TryAddTag(aDidlTag, aNs, Brx::Empty(), valEscaped);
-}
 
-void TidalMetadata::TryAddTagsFromArray(JsonParser& aParser,
-                                     const Brx& aTidalKey, const Brx& aDidlTag,
-                                     const Brx& aNs, const Brx& aRole)
-{
-    if (!aParser.HasKey(aTidalKey)) {
-        return;
-    }
-    auto parserArray = JsonParserArray::Create(aParser.String(aTidalKey));
-    if (parserArray.Type() == JsonParserArray::ValType::Null) {
-        return;
-    }
-    try {
-        for (;;) {
-            Brn val = parserArray.NextString();
-            Bwn valEscaped(val.Ptr(), val.Bytes(), val.Bytes());
-            Json::Unescape(valEscaped);
-            TryAddTag(aDidlTag, aNs, aRole, valEscaped);
-        }
-    }
-    catch (JsonArrayEnumerationComplete&) {}
-}
+    Bwh artworkUri(1024);
+    Bwh baseArtworkUri(1024);
+    baseArtworkUri.Replace(kImageResourceBaseUrl);
 
-void TidalMetadata::TryAddTag(JsonParser& aParser, const Brx& aTidalKey,
-                           const Brx& aDidlTag, const Brx& aNs)
-{
-    if (!aParser.HasKey(aTidalKey)) {
-        return;
+    Parser idParser(aParser.String("cover"));
+    while(!idParser.Finished()) {
+        baseArtworkUri.AppendThrow(idParser.Next('-')); // replace '-' with '/' in value
+        baseArtworkUri.AppendThrow(Brn("/"));
     }
-    Brn val = aParser.String(aTidalKey);
-    Bwn valEscaped(val.Ptr(), val.Bytes(), val.Bytes());
-    Json::Unescape(valEscaped);
-    TryAddTag(aDidlTag, aNs, Brx::Empty(), valEscaped);
-}
 
-void TidalMetadata::TryAddTag(const Brx& aDidlTag, const Brx& aNs,
-                           const Brx& aRole, const Brx& aValue)
-{
-    TryAppend("<");
-    TryAppend(aDidlTag);
-    TryAppend(" xmlns:");
-    TryAppend(aNs);
-    if (aRole.Bytes() > 0) {
-        TryAppend(" role=\"");
-        TryAppend(aRole);
-        TryAppend("\"");
-    }
-    TryAppend(">");
-    WriterBuffer writer(iMetaDataDidl);
-    Converter::ToXmlEscaped(writer, aValue);
-    TryAppend("</");
-    TryAppend(aDidlTag);
-    TryAppend(">");
-}
+    auto writeArtwork = [&] (const Brx& aResolution) {
+        artworkUri.ReplaceThrow(baseArtworkUri);
+        artworkUri.AppendThrow(aResolution);
+        artworkUri.AppendThrow(kImageResourceExtension);
 
-void TidalMetadata::TryAppend(const TChar* aStr)
-{
-    Brn buf(aStr);
-    TryAppend(buf);
-}
+        aWriter.WriteArtwork(artworkUri);
+    };
 
-void TidalMetadata::TryAppend(const Brx& aBuf)
-{
-    if (!iMetaDataDidl.TryAppend(aBuf)) {
-        THROW(BufferOverflow);
-    }
+    writeArtwork(kImageResourceResolutionLow);
+    writeArtwork(kImageResourceResolutionMed);
+    writeArtwork(kImageResourceResolutionHigh);
 }
 
 const Brx& TidalMetadata::IdTypeToString(EIdType aType)
--- OpenHome/Av/Tidal/TidalMetadata.h
+++ OpenHome/Av/Tidal/TidalMetadata.h
@@ -2,6 +2,7 @@
 
 #include <OpenHome/Types.h>
 #include <OpenHome/Buffer.h>
+#include <OpenHome/Av/OhMetadata.h>
 #include <OpenHome/Private/Standard.h>
 #include <OpenHome/Media/Pipeline/Msg.h>
 
@@ -15,9 +16,6 @@ namespace Av {
 
 class TidalMetadata : private OpenHome::INonCopyable
 {
-    static const OpenHome::Brn kNsDc;
-    static const OpenHome::Brn kNsUpnp;
-    static const OpenHome::Brn kNsOh;
     static const OpenHome::Brn kImageResourceBaseUrl;
     static const OpenHome::Brn kImageResourceResolutionLow;
     static const OpenHome::Brn kImageResourceResolutionMed;
@@ -60,12 +58,6 @@ public:
         eSmartRising,
         eSmartDiscovery,
     };
-    enum EImageResolution {
-        eNoImage,
-        eLow,
-        eMed,
-        eHigh
-    };
 public:
     TidalMetadata(OpenHome::Media::TrackFactory& aTrackFactory);
     OpenHome::Media::Track* TrackFromJson(const OpenHome::Brx& aMetadata,
@@ -75,21 +67,9 @@ public:
 private:
     void ParseTidalMetadata(const OpenHome::Brx& aMetadata,
                             const OpenHome::Brx& aTokenId);
-    void TryAddAttribute(OpenHome::JsonParser& aParser,
-                         const TChar* aTidalKey, const TChar* aDidlAttr);
-    void TryAddAttribute(const TChar* aValue, const TChar* aDidlAttr);
-    void TryAddTagsFromArray(OpenHome::JsonParser& aParser,
-                             const OpenHome::Brx& aTidalKey, const OpenHome::Brx& aDidlTag,
-                             const OpenHome::Brx& aNs, const OpenHome::Brx& aRole);
-    void TryAddTagsFromObj(OpenHome::JsonParser& aParser,
-                             const OpenHome::Brx& aTidalKey, const OpenHome::Brx& aDidlTag,
-                             const OpenHome::Brx& aNs, const Brx& aTidalSubKey, TBool aIsImage, EImageResolution aResolution);
-    void TryAddTag(OpenHome::JsonParser& aParser, const OpenHome::Brx& aTidalKey,
-                   const OpenHome::Brx& aDidlTag, const OpenHome::Brx& aNs);
-    void TryAddTag(const OpenHome::Brx& aDidlTag, const OpenHome::Brx& aNs,
-                   const OpenHome::Brx& aRole, const OpenHome::Brx& aValue);
-    void TryAppend(const TChar* aStr);
-    void TryAppend(const OpenHome::Brx& aBuf);
+
+    void TryWriteArtwork(WriterDIDLLite& aWriter,
+                         JsonParser& aParser);
 private:
     OpenHome::Media::TrackFactory& iTrackFactory;
     OpenHome::Media::BwsTrackUri iTrackUri;
