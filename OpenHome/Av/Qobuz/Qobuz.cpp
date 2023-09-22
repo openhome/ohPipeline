1d2b49b72443f0308b91adec4b3e7853c64c2bd7
Ewan <git@ewan.emccartney.co.uk>
2023-09-22 09:38:38 +0100

    Qobuz: Prevent exception thrown when a 'free' account logs in due to missing credential ID.

--- OpenHome/Av/Qobuz/Qobuz.cpp
+++ OpenHome/Av/Qobuz/Qobuz.cpp
@@ -654,7 +654,13 @@ TBool Qobuz::TryLoginLocked()
             iUserId = parserUser.Num("id");
             JsonParser parserCred;
             parserCred.Parse(parserUser.String("credential"));
-            iCredentialId = parserCred.Num("id");
+
+            if (!parserCred.IsNull("id")) {
+                iCredentialId = parserCred.Num("id");
+            }
+            else {
+                LOG(kPipeline, "Qobuz: Returned user has no 'CredentialId' present. Assuming no active subscription and defaulting to '%d'\n", iCredentialId);
+            }
         }
         catch (Exception& ex) {
             LOG_ERROR(kPipeline, "Exception - %s - parsing credentialId during Qobuz login.  Login response is:\n%.*s\n", ex.Message(), PBUF(resp));
