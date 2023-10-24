1c4e8233c83fd62ad21745b30b12744cde0a7699
Lewis Ross <lewis.ross@linn.co.uk>
2023-10-24 17:34:58 +0100

    Roon: Use IAudioTime for debugging start times

--- OpenHome/Av/Raat/Output.cpp
+++ OpenHome/Av/Raat/Output.cpp
@@ -347,17 +347,9 @@ RC__Status RaatOutput::TeardownStream(int aToken)
 
 RC__Status RaatOutput::StartStream(int aToken, int64_t aWallTime, int64_t aStreamTime, RAAT__Stream* aStream)
 {
-    {
-        TUint64 localTime = MclkToNs();
-        LOG(kRaat, "RaatOutput::StartStream(%d, %lld, %lld, %p) iToken=%d, localTime=%llu\n",
-                    aToken, aWallTime, aStreamTime, aStream, iToken, localTime);
-
-        const TUint msPerNs = 1000000;
-        const TUint timeNowMs = localTime / msPerNs;
-        const TUint timeStartMs = aWallTime / msPerNs;
-        const TUint deltaMs = timeStartMs - timeNowMs;
-        Log::Print("[RAAT_TIME] RaatOutput::StartStream()   Time now: %ums, Start at: %ums (%ums)\n", timeNowMs, timeStartMs, deltaMs);
-    }
+    TUint64 localTime = MclkToNs();
+    LOG(kRaat, "RaatOutput::StartStream() aWallTime=%lld, localTime=%llu\n", aWallTime, localTime);
+    iAudioTime.TimerStartTimer(iSampleRate, aWallTime / 1000);
 
     if (aToken != iToken) {
         return RAAT__OUTPUT_PLUGIN_STATUS_INVALID_TOKEN;
