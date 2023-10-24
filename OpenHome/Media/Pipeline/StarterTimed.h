b93f7ffe98ee14f6f92c39ee8f03f2ff14ef01c5
Lewis Ross <lewis.ross@linn.co.uk>
2023-10-24 17:58:08 +0100

    Pipeline: Fix unreferenced formal parameter in StarterTimed for Windows builds

--- OpenHome/Media/Pipeline/StarterTimed.h
+++ OpenHome/Media/Pipeline/StarterTimed.h
@@ -72,8 +72,8 @@ public:
 private: // from IAudioTime
     void GetTickCount(TUint aSampleRate, TUint64& aTicks, TUint& aFrequency) const override;
     void SetTickCount(TUint64 aTicks) override;
-    void TimerStartTimer(TUint aSampleRate, TUint64 aStartTime) override {}
-    void TimerLogTime(const TChar* aId) override {}
+    void TimerStartTimer(TUint /*aSampleRate*/, TUint64 /*aStartTime*/) override {}
+    void TimerLogTime(const TChar* /*aId*/) override {}
 private:
     OsContext* iOsCtx;
     TInt64 iTicksAdjustment;
