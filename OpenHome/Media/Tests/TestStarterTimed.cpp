6885c9b933229adf14130df0ace903af1bd38402
Lewis Ross <lewis.ross@linn.co.uk>
2023-12-14 15:08:34 +0000

    Pipeline: Remove timing debug infrastructure from Raat::Output and StarterTimed

--- OpenHome/Media/Tests/TestStarterTimed.cpp
+++ OpenHome/Media/Tests/TestStarterTimed.cpp
@@ -57,8 +57,6 @@ private: // from IMsgProcessor
 private: // from IAudioTime
     void GetTickCount(TUint aSampleRate, TUint64& aTicks, TUint& aFrequency) const override;
     void SetTickCount(TUint64 aTicks) override;
-    void TimerStartTimer(TUint /*aSampleRate*/, TUint64 /*aStartTime*/) override {}
-    void TimerLogTime(const TChar* /*aId*/) override {}
 private: // from IPipelineAnimator
     TUint PipelineAnimatorBufferJiffies() const override { return 0; }
     TUint PipelineAnimatorDelayJiffies(AudioFormat /*aFormat*/, TUint /*aSampleRate*/, TUint /*aBitDepth*/, TUint /*aNumChannels*/) const override { return 0; }
