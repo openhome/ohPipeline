3b82797e3fbf1907e12763bbb26540a66b9df5bf
Bryan Tester <bryant@linn.co.uk>
2024-12-17 16:00:32 +0000

    fix type in jenkins script

--- projectdata/build_behaviour.py
+++ projectdata/build_behaviour.py
@@ -165,7 +165,7 @@ def configure(context):
     if platform.system() == 'Darwin':
         context.env['CC']  = 'clang'
         context.env['CXX'] = 'clang++' 
-    if "PLATFORM" in context.env and context.env["PLATFORM"] in ["Linux-armhf"]
+    if "PLATFORM" in context.env and context.env["PLATFORM"] in ["Linux-armhf"]\
      and "LEGACY_BUILD" in context.env and context.env["LEGACY_BUILD"].ower() in ["true", "yes", "1"]:
         context.configure_args.append("--legacy")
     python("waf", "configure", context.configure_args)
